df152eb82d5d15b9a4d36ec39b04c65f5e409e81
duythien2212 <112065049+duythien2212@users.noreply.github.com>
2024-11-14 15:22:42 +0000

    create app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,111 @@
 import streamlit as st
 import pandas as pd
 import math
+import numpy as np
+from sklearn.neighbors import KernelDensity
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Anomaly Detection with KDE',
+    page_icon=':mag:', # This is an emoji shortcode. Could be a URL too.
 )
-
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
 
 # Set the title that appears at the top of the page.
 '''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
+# :mag: Anomaly Detection with KDE
 '''
+
+def random_values(n, min, max):
+    return np.random.rand(n)*(max-min) + min
+
+np.random.seed(seed=0)
 
 # Add some spacing
 ''
 ''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
+min_value = -30
+max_value = 30
+min_value, max_value = st.slider(
+    'Select min and max value',
+    min_value=-30,
+    max_value=30,
     value=[min_value, max_value])
 
-countries = gdp_df['Country Code'].unique()
+# st.write(f"{min_value}, {max_value}")
+threadhold_percent = 10
+bandwidth = 1
 
-if not len(countries):
-    st.warning("Select at least one country")
+n_cluster = 1
+n_cluster = st.number_input("Select number of cluster", value=n_cluster, step=1, min_value=1)
 
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+cols = st.columns(n_cluster, gap="medium")
+pts_min = [min_value for i in range(n_cluster)]
+pts_max = [max_value for i in range(n_cluster)]
+n_pts = [1 for i in range(n_cluster)]
+clusters = [[] for i in range(n_cluster)]
+
+
+for i in range(n_cluster):
+    with cols[i]:
+        pts_min[i],pts_max[i] = st.slider(f"Min and max value for {i}th cluster", 
+                                    min_value=min_value, max_value=max_value,
+                                    value=[pts_min[i],pts_max[i]])
+        n_pts[i] = st.number_input("number of point", value=n_pts[i], key=f"{i}_3")
+        clusters[i] = random_values(n_pts[i], pts_min[i], pts_max[i])
+
+pts = np.concatenate(clusters)
+anomaly_pts = np.random.rand(int(pts.size*threadhold_percent/100))*(max_value-min_value) + min_value
+pts = np.concatenate([pts, anomaly_pts])
+
+
 
 ''
 ''
-''
 
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
 
-st.header('GDP over time', divider='gray')
+bandwidth = st.slider(
+    'Select bandwidth',
+    min_value=1,
+    max_value=100,
+    value=bandwidth)
 
-''
+threadhold_percent = st.slider(
+    'Select anomaly threadhold (percent)',
+    min_value=1,
+    max_value=100,
+    value=threadhold_percent)
 
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
+kernel = st.selectbox(
+    "Select kernel",
+    ('gaussian', 'tophat', 'epanechnikov', 'exponential', 'linear', 'cosine'),
 )
 
-''
-''
+kde = KernelDensity(kernel=kernel, bandwidth=bandwidth)  # Chọn kernel Gaussian và băng thông là 0.5
+kde.fit(pts.reshape(-1, 1))  # Reshape data thành dạng 2D cho scikit-learn
+log_dens = kde.score_samples(pts.reshape(-1, 1))
+threshold = np.percentile(log_dens, threadhold_percent)
+outliers = pts[log_dens < threshold]
+
+# Vẽ mật độ KDE
+fig, ax = plt.subplots()
+
+x_grid = np.linspace(min_value, max_value, 1000)
+log_dens_grid = kde.score_samples(x_grid.reshape(-1, 1))
+ax.plot(x_grid, np.exp(log_dens_grid), label='KDE Density Estimation', color='blue')
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+# Vẽ các điểm dữ liệu bình thường
+ax.scatter(pts, np.zeros_like(pts), c='green', label='Normal Data', alpha=0.5)
 
-st.header(f'GDP in {to_year}', divider='gray')
+# Vẽ các điểm bất thường
+ax.scatter(outliers, np.zeros_like(outliers), c='red', label='Anomalies', alpha=1)
 
-''
+# Thêm nhãn và tiêu đề
+ax.legend()
 
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+st.pyplot(fig)
